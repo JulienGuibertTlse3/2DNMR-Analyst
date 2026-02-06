7cc3e55e3c700ac384b1320989a50b3d311db65f
Julien GUIBERT <Julien.Guibert3@outlook.fr>
2026-02-06 18:52:57 +0100

    Add files via upload

--- 2DNMR-Analyst_sans_CNN/Shine.R
+++ 2DNMR-Analyst_sans_CNN/Shine.R
@@ -1175,7 +1175,7 @@ server <- function(input, output, session) {
                   tags$li("Click 'Apply' to confirm changes before exporting"),
                   tags$li("Use the 'Data' tab to review and select boxes for editing"),
                   tags$li("If you want to process a batch, you might want to select the folder, only select a QC or the most intense spectrum. Process it (Step 1), then reload every spectra and use the 'batch export' with the peaks selected on the first spectrum."),
-                  tags$li("For batch treatment, limit the number of sprectrum per batch to 25 for TOCSY, 50 for COSY and HSQC.")
+                  tags$li("For batch treatment, limit the number of spectrum per batch to 25 for TOCSY, 50 for COSY and HSQC.")
                   
           )
           
